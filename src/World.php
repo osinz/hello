5acd2668053154d8db5590f57f8a87d658bab148
osinz <490971501@qq.com>
2016-04-01 23:45:46 +0800

    提交

---
+++ src/World.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Osinz\Hello;
+/**
+ * Dotenv.
+ *
+ * Loads a `.env` file in the given directory and sets the environment vars.
+ */
+class World
+{
+    public function say()
+    {
+        echo 'hello,world';
+    }
+}
